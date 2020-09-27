7afecaf6f9bf8eb6f3809d324ca9ad774c19426e
326th <minecraftskinman@gmail.com>
2020-09-28 02:42:56 +0700

    submit finished work

---
+++ src/main/java/atm/config/Config.java
@@ -0,0 +1,25 @@
+package atm.config;
+
+import atm.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public DataSource dataSource(){
+        return new DataSourceFile("customers.txt");
+    }
+    @Bean
+    public Bank bank(){
+        return new Bank("My Bank", dataSource());
+    }
+    @Bean
+    public ATM atm(){
+        return new ATM(bank());
+    }
+    @Bean
+    public AtmUI atmUI(){
+        return new AtmUI(atm());
+    }
+}
